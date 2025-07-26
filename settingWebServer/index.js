f799a6988697214be25441ac7f820a28fa06100a
Andrea Brion <71452563+AndreaBrion@users.noreply.github.com>
2025-07-26 17:04:39 +0200

    Update

--- settingWebServer/index.js
+++ settingWebServer/index.js
@@ -34,8 +34,8 @@ app.get('/', async (req, res) => {
 });
 */
 
-//Query d'esempio per ottenere i dati da "titleAkas"
-// app.get('/akas', async (req, res) => {                 //Qui da prompt dovevo inserire il nome akas: http://localhost:3000/akas
+/*Query d'esempio per ottenere i dati da "titleAkas
+ app.get('/akas', async (req, res) => {                 //Qui da prompt dovevo inserire il nome akas: http://localhost:3000/akas
 app.get('/', async (req, res) => {
   try {
     const result = await pool.query(`
@@ -49,8 +49,58 @@ app.get('/', async (req, res) => {
     res.status(500).send('Errore nel server o nel database');
   }
 });
+*/
+
+
+/*
+Query che recupera titoli cinematografici associati a un 
+elenco di persone specifiche, mostrando le informazioni sia della persona che del titolo
+*/
+app.get('/test', async (req, res) => {                      //'/' è posto in path della http request in jmeter
+  // const N = 20;
+  const N = 1;  //Dato che via jMeter impostiamo il # di iterazioni
+  const responses = []; // corretto: nome coerente
+  const query = `SELECT DISTINCT
+                        nb."primaryName",
+                        nb."primaryProfession",
+                        tb."originalTitle",
+                        tb."primaryTitle",
+                        tb."startYear"
+                FROM "nameBasic" nb
+                JOIN LATERAL unnest(string_to_array(nb."knownForTitles", ',')) AS known_title("tconst") ON true
+                JOIN "titleBasic" tb ON tb."tconst" = known_title."tconst"
+                WHERE nb."primaryName" IN (
+                                            'Smriti Mishra',
+                                            'Danny Malone',
+                                            'Marcelo del Mestre',
+                                            'Bruce Walton',
+                                            'Sébastien Duncan',
+                                            'Gunter Linke',
+                                            'Carlos Medina',
+                                            'Emily Vaartstra',
+                                            'Uriel Ramos',
+                                            'Mugsy McGuire'
+                                          );`;
+
+  try {
+    for (let i = 0; i < N; i++) {
+      const result = await pool.query(query);
+      responses.push({
+        iteration: i + 1,
+        rowCount: result.rowCount,
+        rows: result.rows
+      });
+    }
+    res.json(responses); 
+  } catch (err) {
+    console.error('Errore nella query SQL:', err);
+    res.status(500).send('Errore nel server o nel database');
+  }
+});
+
 
 // Avvio del server
 app.listen(port, () => {
   console.log(`Server avviato su http://localhost:${port}`);
 });
+
